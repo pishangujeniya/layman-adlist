467393d82a833b880786cf9966054a43cebac992
Pishang Ujeniya <pishangujeniya@outlook.com>
2022-09-23 07:30:58 +0530

    initial commit

---
+++ download_adlists_hosts.sh
@@ -0,0 +1,13 @@
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -o ./steven_black_hosts.txt
+wget https://block.energized.pro/ultimate/formats/hosts.txt -o ./energized_ultimate_hosts.txt
+wget https://adaway.org/hosts.txt -o ./adaway_hosts.txt
+wget https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Hosts/GoodbyeAds.txt -o ./good_bye_ads_hosts.txt
+wget https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-YouTube-AdBlock.txt -o ./good_bye_ads_yt_hosts.txt
+wget https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-Xiaomi-Extension.txt -o ./good_bye_ads_mi_hosts.txt
+wget https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-Samsung-AdBlock.txt -o ./good_bye_ads_samsung_hosts.txt
+wget https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-Spotify-AdBlock.txt -o ./good_bye_ads_spotify_hosts.txt
+wget https://dbl.oisd.nl/ -o ./oisd_nl_hosts.txt
+wget https://www.github.developerdan.com/hosts/lists/amp-hosts-extended.txt -o ./developerdan_amp_extended_hosts.txt
+wget https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt -o ./developerdan_ads_and_tracking_extended_hosts.txt
+wget https://gitlab.com/Dev-i-l/the-quantum-ad-list/-/raw/master/For%20hosts%20file/The_Quantum_Ad-List.txt?inline=true -o ./quantum_ads_hosts.txt
+echo "================================Dowload Complete================================"
