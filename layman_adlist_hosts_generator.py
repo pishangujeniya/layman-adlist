171df0268af1b314fbf89de282a39b56d5da8d74
Pishang Ujeniya <pishangujeniya@outlook.com>
2022-09-24 13:11:52 +0530

    file writing optimised

--- layman_adlist_hosts_generator.py
+++ layman_adlist_hosts_generator.py
@@ -1,11 +1,8 @@
 import argparse
 import os
-from python_hosts import Hosts, HostsEntry
+from python_hosts import Hosts
 
-# Initialize parser
 parser = argparse.ArgumentParser()
-
-# Adding optional argument
 parser.add_argument("-d",
                     "--dir",
                     required=True,
@@ -22,6 +19,7 @@ args = parser.parse_args()
 hosts_dir_path = args.dir
 output_hosts_file_absolute_path = args.out
 
+# Getting Hosts File Paths
 hosts_files_path = []
 print("Generating using the following files in : ", hosts_dir_path)
 for entry in os.scandir(hosts_dir_path):
@@ -30,6 +28,7 @@ for entry in os.scandir(hosts_dir_path):
             os.path.abspath(hosts_dir_path + os.path.sep + entry.name))
         print(os.path.abspath(hosts_dir_path + os.path.sep + entry.name))
 
+# Reading Domains from all hosts files
 print("Extracting Domains")
 hosts_domains_list = []
 for host_file_path in hosts_files_path:
@@ -40,34 +39,29 @@ for host_file_path in hosts_files_path:
                 hosts_domains_list.append(domain)
 print("Extracted Domains Count : ", len(hosts_domains_list))
 
+# Filtering only Unique domains
 print("Listing Unique Domains")
 hosts_domains_list = list(set(hosts_domains_list))
 print("Unique Domains Count : ", len(hosts_domains_list))
 
-print("Generating Layman Adlists Hosts File at : ",
-      output_hosts_file_absolute_path)
-layman_adlists_hosts = Hosts(output_hosts_file_absolute_path)
+# Generating Host file content
+print("Generating Layman Adlists Hosts File Content")
+hosts_lines = []
+hosts_lines.append('# Layman Adlist')
+hosts_lines.append('\n')
+hosts_lines.append('# https://github.com/pishangujeniya/layman-adlist')
+hosts_lines.append('\n')
+hosts_lines.append(
+    '# https://raw.githubusercontent.com/pishangujeniya/layman-adlist/main/layman_adlist_hosts.txt')
+hosts_lines.append('\n')
+hosts_lines.append('\n')
 
-# Generating Hosts File Comment
-layman_adlists_hosts.add(
-    [HostsEntry(entry_type='comment', comment='Layman Adlists')])
-layman_adlists_hosts.add([
-    HostsEntry(entry_type='comment',
-               comment='https://github.com/pishangujeniya/layman-adlist')
-])
-layman_adlists_hosts.add([
-    HostsEntry(
-        entry_type='comment',
-        comment=
-        'https://raw.githubusercontent.com/pishangujeniya/layman-adlist/master/layman_adlist_hosts.txt'
-    )
-])
-layman_adlists_hosts.add(
-    [HostsEntry(entry_type='comment', comment='License: MIT License')])
+for i in hosts_domains_list:
+    hosts_lines.append('\n127.0.0.1 '+i)
+hosts_lines.append('\n')
 
-for domain in hosts_domains_list:
-    new_entry = HostsEntry(entry_type='ipv4',
-                           address='127.0.0.1',
-                           names=[domain])
-    layman_adlists_hosts.add([new_entry])
-layman_adlists_hosts.write()
+# Writing Hosts file
+print("Writing hosts file : ", output_hosts_file_absolute_path)
+file_object = open(output_hosts_file_absolute_path, 'w')
+file_object.writelines(hosts_lines)
+file_object.close()
--- layman_adlist_hosts_generator.sh
+++ layman_adlist_hosts_generator.sh
@@ -15,8 +15,8 @@ developerdan_ads_and_tracking_extended_hosts="developerdan_ads_and_tracking_exte
 quantum_ads_hosts="quantum_ads_hosts.txt"
 
 # Merged Single Layman Adlists hosts file
-layman_adlists_hosts="layman_adlists_hosts.txt"
+layman_adlist_hosts="layman_adlist_hosts.txt"
 
 # Logic
 cd ${DownloadDirPath}
-cat ${steven_black_hosts} ${energized_ultimate_hosts} ${adaway_hosts} ${good_bye_ads_hosts} ${good_bye_ads_yt_hosts} ${good_bye_ads_mi_hosts} ${good_bye_ads_samsung_hosts} ${good_bye_ads_spotify_hosts} ${oisd_nl_hosts} ${developerdan_amp_extended_hosts} ${developerdan_ads_and_tracking_extended_hosts} ${quantum_ads_hosts} > ./../${layman_adlists_hosts}
+cat ${steven_black_hosts} ${energized_ultimate_hosts} ${adaway_hosts} ${good_bye_ads_hosts} ${good_bye_ads_yt_hosts} ${good_bye_ads_mi_hosts} ${good_bye_ads_samsung_hosts} ${good_bye_ads_spotify_hosts} ${oisd_nl_hosts} ${developerdan_amp_extended_hosts} ${developerdan_ads_and_tracking_extended_hosts} ${quantum_ads_hosts} > ./../${layman_adlist_hosts}
